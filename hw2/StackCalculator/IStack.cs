932f0966aa400f20b340ffec3d4c27e1bd93d375
w1ndok814 <pavelpavelrubanov@gmail.com>
2023-03-16 21:09:42 +0300

    StackCalculator without tests

---
+++ hw2/StackCalculator/IStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCalculator
+{
+    /// <summary>
+    /// Classic stack with double elements
+    /// </summary>
+    public interface IStack
+    {
+        /// <summary>
+        /// Add an element to the top of the stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(double value);
+        /// <summary>
+        /// Remove and returns an element from the top of the stack
+        /// </summary>
+        /// <returns></returns>
+        public double Pop();
+        /// <summary>
+        /// Returns an element from the top of the stack
+        /// </summary>
+        /// <returns></returns>
+        public double Top();
+        /// <summary>
+        /// Return true if stack contains 0 elements
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty();
+    }
+}
---
+++ hw2/StackCalculator/StackCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCalculator
+{
+    public static class StackCalculator
+    {
+        public static double Calculate(string expression, IStack stack)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var dictionary = "0123456789/*-+ ";
+            if (!(expression.All(el => dictionary.Contains(el))))
+            {
+                throw new ArgumentException("Incorrect symbols in expression");
+            }
+
+            var operations = "/*-+";
+            foreach (var element in expression.Split(" "))
+            {
+                if (double.TryParse(element, out double number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    if (operations.Contains(element))
+                    {
+                        try
+                        {
+                            var number1 = stack.Pop();
+                            var number2 = stack.Pop();
+                            switch(element)
+                            {
+                                case "+":
+                                    stack.Push(number1 + number2);
+                                    break;
+                                case "-":
+                                    stack.Push(number1 - number2);
+                                    break;
+                                case "*":
+                                    stack.Push(number1 * number2);
+                                    break;
+                                case "/":
+                                    if (Math.Abs(number2) < 1e-9)
+                                    {
+                                        throw new DivideByZeroException();
+                                    }
+                                    break;
+                            }
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            throw new ArgumentException("Incorrect expression");
+                        }
+                    }
+                }
+            }
+            var result = stack.Pop();
+            if (!stack.IsEmpty())
+            {
+                throw new ArgumentException("Incorrect expression");
+            }
+            return result;
+        }
+    }
+}
