fcaa06c46007895048745bfcf4bb73b83a7291b5
w1ndok814 <pavelpavelrubanov@gmail.com>
2023-03-17 21:28:22 +0300

    fixed tests

--- hw2/StackCalculator/StackCalculatorTests/StackCalculatorTests.cs
+++ hw2/StackCalculator/StackCalculatorTests/StackCalculatorTests.cs
@@ -4,53 +4,57 @@ namespace StackCalculator.StackCalculatorTests
 {
     public class Tests
     {
-        private IStack stack;
         [SetUp]
         public void Setup()
         {
-            stack = new StackOnLinkedList();
-            //stack = new StackOnList();
-        }
 
-        [Test]
-        public void NullExpressionShouldThrowArgumentNullException()
+        }
+        private static IEnumerable<TestCaseData> Stacks
+            => new TestCaseData[]
+                {
+                    new TestCaseData(new StackOnLinkedList()),
+                    new TestCaseData(new StackOnList()),
+                };
+
+        [TestCaseSource(nameof(Stacks))]
+        public void NullExpressionShouldThrowArgumentNullException(IStack stack)
         {
             Assert.Catch<ArgumentNullException>(() => StackCalculator.Calculate(null, stack));
         }
 
-        [Test]
-        public void EmptyExpressionShouldThrowArgumentException()
+        [TestCaseSource(nameof(Stacks))]
+        public void EmptyExpressionShouldThrowArgumentException(IStack stack)
         {
             Assert.Catch<ArgumentException>(() => StackCalculator.Calculate("", stack));
         }
 
-        [Test]
-        public void NotEmptyStackShouldThrowArgumentException()
+        [TestCaseSource(nameof(Stacks))]
+        public void NotEmptyStackShouldThrowArgumentException(IStack stack)
         {
             stack.Push(1);
             Assert.Catch<ArgumentException>(() => StackCalculator.Calculate("2 2 +", stack));
         }
 
-        [Test]
-        public void DivideByZeroShouldThrowDivideByZeroException()
+        [TestCaseSource(nameof(Stacks))]
+        public void DivideByZeroShouldThrowDivideByZeroException(IStack stack)
         {
             Assert.Catch<DivideByZeroException>(() => StackCalculator.Calculate("2 0 /", stack));
         }
 
-        [Test]
-        public void ExtraNumberInExpressionShouldThrowArgumentException()
+        [TestCaseSource(nameof(Stacks))]
+        public void ExtraNumberInExpressionShouldThrowArgumentException(IStack stack)
         {
             Assert.Catch<ArgumentException>(() => StackCalculator.Calculate("2 2 0 0 0 +", stack));
         }
 
-        [Test]
-        public void ExtraOperationsInExpressionShouldThrowArgumentException()
+        [TestCaseSource(nameof(Stacks))]
+        public void ExtraOperationsInExpressionShouldThrowArgumentException(IStack stack)
         {
             Assert.Catch<ArgumentException>(() => StackCalculator.Calculate("2 2 + +", stack));
         }
 
-        [Test]
-        public void StandartExpessionShouldGiveRightAnswer()
+        [TestCaseSource(nameof(Stacks))]
+        public void StandartExpessionShouldGiveRightAnswer(IStack stack)
         {
             Assert.That(StackCalculator.Calculate("1 2 3 + *", stack),Is.EqualTo(5));
         }
